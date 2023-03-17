d79d14c95829541b5e92ceb802c3c610a373840c
aroxx1 <choro7atarim@gmail.com>
2023-03-16 17:49:35 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#inclide < stdio.h >
+#include <stdio.h>
+/**
+ *main - entry poit
+ *return: always 0 (success)
+ */
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
